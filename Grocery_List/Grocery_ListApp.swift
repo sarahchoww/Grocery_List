416b3dbf0ae9624379e4d70fd2541d7d4ea7a490
sarahchoww <chow.sarah@hotmail.com>
2022-01-22 20:43:06 -0500

    Initial Commit

---
+++ Grocery_List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery_List
+//
+//  Created by Sarah Chow on 2022-01-22.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
