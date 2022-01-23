04578e0893a843e297bed77b65fe1b77184e5bdc
sarahchoww <chow.sarah@hotmail.com>
2022-01-23 11:47:39 -0500

    new files, creating

--- Grocery_List/CreateListView.swift
+++ Grocery_List/CreateListView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct CreateListView: View {
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Add Item"){
+            NavigationLink(destination: CreateItemView())
+        }
     }
 }
 
